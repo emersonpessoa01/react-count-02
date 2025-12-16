428f86e1f61c89c84343d2d704008f7a7f6dc05e
𝐄𝐦𝐞𝐫𝐬𝐨𝐧 𝐏𝐞𝐬𝐬𝐨𝐚 <60630092+emersonpessoa01@users.noreply.github.com>
2025-12-16 15:39:15 -0300

    chore: Atualizado index.js para compatibilidade com React 18 :tada:

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-import 'materialize-css/dist/css/materialize.min.css';
-import './index.css';
+import "materialize-css/dist/css/materialize.min.css";
+import "./index.css";
 
-import App from './App';
+import App from "./App";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
